7777da9e5c7a5d36cbe727ed4b7b5fb004f4afcf
Moises Jimenez <moises.jimenez@partner.bmw.de>
2020-05-15 09:22:53 +0200

    Make Filter in Color uppercase

--- src/color.py
+++ src/color.py
@@ -5,7 +5,7 @@ class Color:
     def __init__(self, img):
         self.s_ = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)[:,:,2]
 
-    def filter(self, threshold=(0, 255)):
+    def Filter(self, threshold=(0, 255)):
         output = np.zeros_like(self.s_)
         output[(self.s_ > threshold[0]) & (self.s_ < threshold[1])] = 1
         return output
--- src/test_color.py
+++ src/test_color.py
@@ -6,7 +6,7 @@ from color import Color
 img = plt.imread('../test_images/test1.jpg')
 color = Color(img)
 
-filtered = color.filter((170, 255))
+filtered = color.Filter((170, 255))
 
 plt.imshow(filtered, cmap='gray')
 plt.show()
