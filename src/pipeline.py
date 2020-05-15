313744bebe6d97857e5ce9a56e78f250b35d6741
Moises Jimenez <moises.jimenez@partner.bmw.de>
2020-05-15 09:24:26 +0200

    Add gradient and color functionality to pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,13 +1,20 @@
 import cv2
 import glob
 import matplotlib.pyplot as plt
+import numpy as np
 
 from calibration import Calibration
+from gradient import Gradient
+from color import Color
 
 class Pipeline:
-    def __init__(self, nx, ny):
+    def __init__(self, nx, ny, kernel_size = 3):
         self.calibration_ = Calibration(nx, ny)
-        self.images_ = []
+        self.img_ = {}
+        self.gradient_ = Gradient(kernel_size)
+        self.gradient_images_ = []
+        self.color_ = {}
+        self.filtered_ = {}
 
     def Calibrate(self, calibration_images_pattern):
         files = glob.glob(calibration_images_pattern)
@@ -17,9 +24,36 @@ class Pipeline:
             self.calibration_.Update(gray)
         self.calibration_.Calibrate()
 
-    def Undistort(self, files_pattern):
-        files = glob.glob(files_pattern)
-        for image in files:
-            img = plt.imread(image)
-            self.images_.append(self.calibration_.Undistort(img))
+    def Undistort(self, img):
+        self.img_ = self.calibration_.Undistort(img)
 
+    def CalculateGradient(self):
+        self.gradient_.CalculateGradient(self.img_)
+
+    def FilterGradients(self, absolute_thresh=(0, 255), magnitude_thresh=(0,255), direction_thresh=(0, np.pi/2)):
+        absolute = self.gradient_.AbsoluteThreshold(absolute_thresh)
+        self.gradient_images_.append(absolute[0])
+        self.gradient_images_.append(absolute[1])
+        self.gradient_images_.append(self.gradient_.MagnitudeThreshold(magnitude_thresh))
+        self.gradient_images_.append(self.gradient_.DirectionThreshold(direction_thresh))
+
+    def InitColor(self):
+        self.color_ = Color(self.img_)
+
+    def FilterColor(self, threshold=(0,255)):
+        self.filtered_ = self.color_.Filter(threshold)
+
+    def JoinOption(self, option = 'A'):
+        combined = np.zeros_like(self.gradient_images_[0])
+        abs_threshold = []
+        abs_threshold.append(self.gradient_images_[0])
+        abs_threshold.append(self.gradient_images_[1])
+        mag_threshold = self.gradient_images_[2]
+        dir_threshold = self.gradient_images_[3]
+        if option is 'A':
+            combined[(((abs_threshold[0] == 1) & (abs_threshold[1] == 1)) | ((mag_threshold == 1) & (dir_threshold == 1))) | (self.filtered_ == 1)] = 1
+        elif option is 'B':
+            combined[(abs_threshold[0] == 1) | (self.filtered_ == 1)] = 1
+        elif option is 'C':
+            combined[(abs_threshold[0] == 1) | (self.filtered_ == 1)] = 1
+        return combined
