206db6cd5ed7034c500cbec89cc457ba3ef55a82
Moises Jimenez <moisesejimenezg@gmail.com>
2020-10-11 12:15:33 +0200

    Extract calibration/perspective transform setup

--- src/pipeline.py
+++ src/pipeline.py
@@ -23,6 +23,9 @@ class Pipeline:
         self.perspective_ = {}
         self.gradient_images_ = []
         self.filtered_ = {}
+        self.calibration_path_ = '../camera_cal/calibration*.jpg'
+        self.curvatures_ = {'left': [], 'right': []}
+
 
     def Calibrate(self, calibration_images_pattern):
         files = glob.glob(calibration_images_pattern)
@@ -32,11 +35,34 @@ class Pipeline:
             self.calibration_.Update(gray)
         self.calibration_.Calibrate()
 
+
+    def InitPerspective(self):
+
+        src_p0 = Point(190, 720)
+        src_p1 = Point(550, 480)
+        src_p2 = Point(730, 480)
+        src_p3 = Point(1115, 720)
+
+        src_trp = Trapezoid(src_p0, src_p1, src_p2, src_p3)
+
+        dst_p0 = Point(250, 720)
+        dst_p1 = Point(250, 460)
+        dst_p2 = Point(980, 460)
+        dst_p3 = Point(980, 720)
+
+        dst_trp = Trapezoid(dst_p0, dst_p1, dst_p2, dst_p3)
+
+        transformation = Transformation(src_trp, dst_trp)
+        self.perspective_ = Perspective(transformation)
+
+
     def Undistort(self, img):
         self.img_ = self.calibration_.Undistort(img)
 
+
     def CalculateGradient(self):
         self.gradient_.CalculateGradient(self.img_)
+
 
     def FilterGradients(self, absolute_thresh=(0, 255), magnitude_thresh=(0, 255), direction_thresh=(0, np.pi/2)):
         absolute = self.gradient_.AbsoluteThreshold(absolute_thresh)
@@ -47,11 +73,14 @@ class Pipeline:
         self.gradient_images_.append(
             self.gradient_.DirectionThreshold(direction_thresh))
 
+
     def InitColor(self):
         self.color_ = Color(self.img_)
 
+
     def FilterColor(self, threshold=(0, 255)):
         self.filtered_ = self.color_.Filter(threshold)
+
 
     def JoinOption(self, option='A'):
         combined = np.zeros_like(self.gradient_images_[0])
@@ -69,50 +98,42 @@ class Pipeline:
             combined[(abs_threshold[0] == 1) | (self.filtered_ == 1)] = 1
         return combined
 
-    def InitPerspective(self, transformation):
-        self.perspective_ = Perspective(transformation)
 
     def Transform(self, img):
         return self.perspective_.Transform(img)
 
+
     def FitPolynomial(self, img, visualize=False):
         return self.lines_.Process(img, visualize)
 
+
     def CalculateCurvature(self, polynomial_fit, y, ym_per_pix=(30/720)):
         return self.lines_.CalculateCurvature(polynomial_fit, y, ym_per_pix)
+
 
     def PlotLaneOnImage(self, img, left_fit, right_fit):
         lane_img = self.lines_.PlotPoly(img, left_fit, right_fit)
         unwarped_lane = self.perspective_.InverseTransform(lane_img)
         return cv2.addWeighted(self.img_, 1, unwarped_lane, 0.3, 0)
 
-    def Process(self, img):
-        plt.imshow(img)
-        plt.show()
+
+    def Prepare(self):
+        self.Calibrate(self.calibration_path_)
+        self.InitPerspective()
+
+
+    def Process(self, img, display = False):
+        self.gradient_images_ = []
+        if display:
+            plt.imshow(img)
+            plt.show()
         self.Undistort(img)
         self.CalculateGradient()
         self.FilterGradients((20, 100), (30, 100), (0.7, 1.3))
         self.InitColor()
         self.FilterColor((170, 255))
-        combinedA = self.JoinOption('A')
+        combinedA = self.JoinOption()
 
-        src_p0 = Point(190, 720)
-        src_p1 = Point(550, 480)
-        src_p2 = Point(730, 480)
-        src_p3 = Point(1115, 720)
-
-        src_trp = Trapezoid(src_p0, src_p1, src_p2, src_p3)
-
-        dst_p0 = Point(250, 720)
-        dst_p1 = Point(250, 460)
-        dst_p2 = Point(980, 460)
-        dst_p3 = Point(980, 720)
-
-        dst_trp = Trapezoid(dst_p0, dst_p1, dst_p2, dst_p3)
-
-        transformation = Transformation(src_trp, dst_trp)
-
-        self.InitPerspective(transformation)
         transformed = self.Transform(combinedA)
 
         left_fit, right_fit = self.FitPolynomial(transformed, False)
@@ -120,7 +141,10 @@ class Pipeline:
         result = self.PlotLaneOnImage(self.img_, left_fit, right_fit)
 
         y_eval = img.shape[0]
-        left_curvature = self.CalculateCurvature(left_fit.rw_polynomial_, y_eval)
-        right_curvature = self.CalculateCurvature(right_fit.rw_polynomial_, y_eval)
+        self.curvatures_['left'].append(self.CalculateCurvature(left_fit.rw_polynomial_, y_eval))
+        self.curvatures_['right'].append(self.CalculateCurvature(right_fit.rw_polynomial_, y_eval))
 
-        return result, left_curvature, right_curvature
+        return result
+
+    def GetCurvatures(self):
+        return self.curvatures_
--- src/test_pipeline.py
+++ src/test_pipeline.py
@@ -7,8 +7,8 @@ from geometry import Trapezoid
 from pipeline import Pipeline
 
 pipeline = Pipeline(9, 6, 15)
-pipeline.Calibrate('../camera_cal/calibration*.jpg')
-img = plt.imread('../test_images/test3.jpg')
+pipeline.Prepare()
+ img = plt.imread('../test_images/test3.jpg')
 pipeline.Undistort(img)
 pipeline.CalculateGradient()
 pipeline.FilterGradients((20, 100), (30, 100), (0.7, 1.3))
@@ -19,23 +19,6 @@ combinedA = pipeline.JoinOption('A')
 combinedB = pipeline.JoinOption('B')
 combinedC = pipeline.JoinOption('C')
 
-src_p0 = Point(190, 720)
-src_p1 = Point(550, 480)
-src_p2 = Point(730, 480)
-src_p3 = Point(1115, 720)
-
-src_trp = Trapezoid(src_p0, src_p1, src_p2, src_p3)
-
-dst_p0 = Point(250, 720)
-dst_p1 = Point(250, 460)
-dst_p2 = Point(980, 460)
-dst_p3 = Point(980, 720)
-
-dst_trp = Trapezoid(dst_p0, dst_p1, dst_p2, dst_p3)
-
-transformation = Transformation(src_trp, dst_trp)
-
-pipeline.InitPerspective(transformation)
 transformed = pipeline.Transform(combinedA)
 
 left_fit, right_fit = pipeline.FitPolynomial(transformed, False)
@@ -43,11 +26,11 @@ left_fit, right_fit = pipeline.FitPolynomial(transformed, False)
 result = pipeline.PlotLaneOnImage(pipeline.img_, left_fit, right_fit)
 
 y_eval = img.shape[0]
-left_curvature = pipeline.CalculateCurvature(left_fit.rw_polynomial_, y_eval)
-right_curvature = pipeline.CalculateCurvature(right_fit.rw_polynomial_, y_eval)
+pipeline.CalculateCurvature(left_fit.rw_polynomial_, y_eval)
+pipeline.CalculateCurvature(right_fit.rw_polynomial_, y_eval)
 
-print('Left curvature: ' + str(left_curvature))
-print('Right curvature: ' + str(right_curvature))
+print('Left curvature: ' + str(pipeline.GetCurvatures()['left']))
+print('Right curvature: ' + str(pipeline.GetCurvatures()['right']))
 
 f, (ax1, ax2) = plt.subplots(2, 2, figsize=(24, 9))
 f.tight_layout()
